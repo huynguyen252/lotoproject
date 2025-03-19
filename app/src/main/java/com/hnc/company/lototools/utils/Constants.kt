59fa4ab16067001bffec36698b1109bd071901a2
hnc <hnc@gmail.com>
2025-03-19 09:32:49 +0700

    hi

--- app/src/main/java/com/hnc/company/lototools/utils/Constants.kt
+++ app/src/main/java/com/hnc/company/lototools/utils/Constants.kt
@@ -1,12 +1,2 @@
 package com.hnc.company.lototools.utils
 
-const val MAX_SIZE_QUANTITY_PRODUCT = 50
-const val MAX_SIZE_PRODUCT = 150
-const val MAX_WEIGHT_PRODUCT = 20
-const val KEY_SELECTED_PRODUCT_RESULT = "KEY_SELECTED_PRODUCT_RESULT"
-
-enum class ValueSelectionType(val title: String){
-    QUANTITY("Chọn số lượng"),
-    WEIGHT("Chọn khối lượng"),
-    PACKAGE("Chọn số kiện"),
-}
